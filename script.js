6f63b6e73723701e1031666c4ca41c26224244b1
Patricia Miscolcz <patriciaampese@hotmail.com>
2026-07-11 00:45:22 -0300

    feat: implementa cadastro de tarefas
    Fixes #4

--- script.js
+++ script.js
@@ -5,11 +5,27 @@ const inputTarefa = document.getElementById("tarefa");
 const botaoAdicionar = document.querySelector("button");
 
 // Área onde as tarefas serão exibidas
-const listaTarefas = document.getElementById("lista");
+const listaTarefas = document.getElementById("listaTarefas");
 
 // Evento de clique
 botaoAdicionar.addEventListener("click", () => {
+    const textoTarefa = inputTarefa.value.trim();
 
-    console.log("Botão foi clicado.");
+if (textoTarefa === "") {
+    alert("Digite uma tarefa.");
+    return;
+}
 
+// Remove a mensagem inicial
+if (listaTarefas.textContent.includes("Nenhuma tarefa")) {
+    listaTarefas.innerHTML = "";
+}
+
+const novaTarefa = document.createElement("p");
+novaTarefa.textContent = textoTarefa;
+
+listaTarefas.appendChild(novaTarefa);
+
+inputTarefa.value = "";
+inputTarefa.focus();
 });
