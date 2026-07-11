4ef9984bff47ae7639eee0cc9aa0a3a4fe6ef46c
Patricia Miscolcz <patriciaampese@hotmail.com>
2026-07-11 00:26:21 -0300

    feat: prepara estrutura JavaScript
    Fixes #3

---
+++ script.js
@@ -0,0 +1,15 @@
+// Campo onde o usuário digita a tarefa
+const inputTarefa = document.getElementById("tarefa");
+
+// Botão Adicionar
+const botaoAdicionar = document.querySelector("button");
+
+// Área onde as tarefas serão exibidas
+const listaTarefas = document.getElementById("lista");
+
+// Evento de clique
+botaoAdicionar.addEventListener("click", () => {
+
+    console.log("Botão foi clicado.");
+
+});
